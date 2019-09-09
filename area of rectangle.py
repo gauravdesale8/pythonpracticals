97a926313435e806b5083831a4ba9e8ad2ff1244
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2019-09-09 21:39:22 +0530

    practical 1

---
+++ area of rectangle.py
@@ -0,0 +1,4 @@
+l=int(input("Enter the Length:-"))
+b=int(input("Enter the Breadth:-"))
+n=l*b
+print("Area of Rectangle:-",n)
---
+++ multiplication of two number.py
@@ -0,0 +1,4 @@
+a=int(input("Enter the Number:-"))
+b=int(input("Enter the Number:-"))
+c=a*b
+print("Multiplication is:-",c)
